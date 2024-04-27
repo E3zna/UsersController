45f0824105b6a841f99adf30b30909cc394e5847
3zna <3zha91@gmail.com>
2024-04-27 15:33:36 +0300

    Initial commit

---
+++ src/main/java/TestAssigment/UsersController/UsersControllerApplication.java
@@ -0,0 +1,13 @@
+package TestAssigment.UsersController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TestAssigment/UsersController/UsersControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package TestAssigment.UsersController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
